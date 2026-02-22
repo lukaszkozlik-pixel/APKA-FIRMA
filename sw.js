918fef2d6e1b673814d962c47e10269d6185135f
lukas <lukaszkozlik4@gmail.com>
2026-02-22 21:57:25 +0100

    usunięcie blokady logowania ze wszystkich stron, zostaje index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'instalator-v2.3.5'; // Zmieniaj przy każdej aktualizacji, np. 'instalator-v2.3.5'
+const CACHE_NAME = 'instalator-v2.3.6'; // Zmieniaj przy każdej aktualizacji, np. 'instalator-v2.3.5'
 
 const ASSETS = [
   'index.html',
