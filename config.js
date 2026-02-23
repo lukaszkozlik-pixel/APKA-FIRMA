48d5bad440a7cfea7b80583656d59aee6fdf84d1
lukas <lukaszkozlik4@gmail.com>
2026-02-23 23:19:46 +0100

    szyfrowanie bazy danych

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 const APP_CONFIG = {
     DB_NAME: "InstalatorDB",
     DB_VERSION: 6, // Zwiększaj przy każdej zmianie struktury bazy
+   
+
+    SECURE_DB_NAME: "InstalatorSecureDB",
+    SECURE_DB_VERSION: 1, // Baza do przechowywania zaszyfrowanych danych
+
     APP_VERSION: "2.3.1", // Zwiększ przy każdej aktualizacji aplikacji
         
 };
