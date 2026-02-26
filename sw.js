a4f5b37fa28a23eba639b73b61c42c1196fd0c93
lukas <lukaszkozlik4@gmail.com>
2026-02-26 22:41:34 +0100

    naprawa generowaniapdf + kompresja zdjęć

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'instalator-v2.4.3'; // Zmieniaj przy każdej aktualizacji, np. 'instalator-v2.3.5'
+const CACHE_NAME = 'instalator-v2.4.4'; // Zmieniaj przy każdej aktualizacji, np. 'instalator-v2.3.5'
 
 const ASSETS = [
   'index.html',
